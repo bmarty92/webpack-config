19e63e40894884cf05a6427328ee9c32ce765f23
bmarty92 <martynas.businskas@gmail.com>
2018-10-23 20:39:35 +0300

    'initial-commit'

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const fs = require('fs');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const BUILD = path.resolve(__dirname, 'build');
+const babelrc = JSON.parse(fs.readFileSync('./.babelrc'));
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[hash].bundle.js',
+    path: BUILD,
+  },
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin(),
+      new UglifyJsPlugin({ parallel: true, sourceMap: true }),
+    ],
+  },
+
+  devtool: 'source-map',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: babelrc,
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(BUILD),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: '[hash].bundle.css',
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: './public',
+        to: BUILD,
+        ignore: ['index.html'],
+      },
+    ]),
+  ],
+};
